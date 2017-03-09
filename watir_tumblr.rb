3dbe4653d89ca7d567458eb2f46b7c85482f4586
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-03-09 14:45:59 +0000

    Post a text post

--- watir_tumblr.rb
+++ watir_tumblr.rb
@@ -3,6 +3,7 @@ require 'watir'
 browser = Watir::Browser.new :chrome
 browser.goto "https://www.tumblr.com/"
 
+# Post a text post
 browser.button(id: "signup_login_button").click
 sleep 2
 browser.text_field(name: 'determine_email').set("mrkhan89@hotmail.co.uk")
@@ -11,6 +12,12 @@ sleep 1
 browser.text_field(id: 'signup_password').set("sparta101")
 browser.button(id: "signup_forms_submit").click
 sleep 2
-browser.button(id: "new_post_label_text").click
+browser.a(id: "new_post_label_text").click
 sleep 2
-browser.text_field(class: 'field').set("sparta101")
+browser.element(css: "div[aria-label='Post title']").send_keys 'Hello'
+sleep 2
+browser.element(css: "div[aria-label='Post body']").send_keys 'The three Amigos!'
+sleep 2
+browser.element(css: "div[aria-label='Post tags']").send_keys 'sparta'
+browser.button(class: "create_post_button").click
+sleep 2
